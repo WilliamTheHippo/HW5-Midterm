bed4e7cb74cb2ad8007eb6f5fd99891fde6bf518
Alex Gould <ag4536@nyu.edu>
2020-10-08 16:12:10 -0400

    beginning of click+drag

---
+++ Assets/Scripts/Draggable.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Draggable : MonoBehaviour
+{
+    Vector2 mousePosition;
+    Vector3 originalPosition;
+	Vector3 mouseOffset;
+	Vector3 screenPosition;
+
+    void OnMouseDown()  //only runs if we're on the collider
+    {
+        originalPosition = transform.position;
+    	mousePosition = Input.mousePosition;
+    	mouseOffset = transform.position - Camera.main.ScreenToWorldPoint(mousePosition);
+    }
+
+    void OnMouseDrag()
+    {
+    	mousePosition = Input.mousePosition;
+    	screenPosition = mousePosition;
+    	transform.position = Camera.main.ScreenToWorldPoint(screenPosition) + mouseOffset;
+    }
+
+    void OnMouseUp()
+    {
+        
+    }
+}
