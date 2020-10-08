ad506d51fbbcb0643140898a5546b94136e95c89
Alex Gould <ag4536@nyu.edu>
2020-10-08 16:39:24 -0400

    more dragging (slow going)

--- Assets/Scripts/Draggable.cs
+++ Assets/Scripts/Draggable.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class Draggable : MonoBehaviour
 {
+    public Sprite dragSprite;
+    Sprite normalSprite;
+    SpriteRenderer sr;
+
     Vector2 mousePosition;
     Vector3 originalPosition;
 	Vector3 mouseOffset;
 	Vector3 screenPosition;
 
-    void OnMouseDown()  //only runs if we're on the collider
+    void Start()
     {
+        sr = GetComponent<SpriteRenderer>();
+        normalSprite = sr.sprite;
+    }
+
+    void OnMouseDown()  //only runs if we're on the collider (also this function apparently uses raycasts)
+    {
+        if(dragSprite != null) sr.sprite = dragSprite;
+        sr.color = new Color(1f,0.7f,0.7f);
         originalPosition = transform.position;
     	mousePosition = Input.mousePosition;
     	mouseOffset = transform.position - Camera.main.ScreenToWorldPoint(mousePosition);
@@ -25,6 +37,11 @@ public class Draggable : MonoBehaviour
 
     void OnMouseUp()
     {
-        
+        sr.sprite = normalSprite;
+        sr.color = new Color(1f,1f,1f);
+        RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position, Vector2.zero);
+        Debug.Log(hits);
+        //TODO add snapback, glowing, and hooks to other scripts
+        //TODO maybe just the first raycast; not sure if we need to see all the way down
     }
 }
