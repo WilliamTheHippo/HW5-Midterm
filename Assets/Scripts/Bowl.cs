331641b9cc632e28cf9dd0482de75ccc345d91ca
Alex Gould <ag4536@nyu.edu>
2020-10-12 12:00:13 -0400

    starting coding

---
+++ Assets/Scripts/Bowl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bowl : MonoBehaviour
+{
+    //"contains" enum shows what kind of food it contains (food enum shared with Ingredient.cs)
+    //draggable enabled only when food is prepped
+    //show prepping with color shift for now, animation later
+
+    public void OnDestroy()
+    {
+    	//respawn contained ingredient
+    	//(Draggable.cs should respawn the bowl)
+    }
+}
